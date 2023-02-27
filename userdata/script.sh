308b0349c71bab8b533109baf07121de03e5e0d9
ugovalemeto <potus-mac@potus-mac.local>
2023-02-27 03:11:43 +0100

    format terraform code

--- userdata/script.sh
+++ userdata/script.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
-sudo amazon-linux-extras install nginx1 -y &&
-sudo amazon-linux-extras install epel -y   &&
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y &&
-sudo yum install certbot python-certbot-nginx &&
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y nginx
 sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo apt install certbot python3-certbot-nginx -y
+
 
 # Set the file name and directory path
 file_name="index.html"
-dir_path="/usr/share/nginx/html"
+dir_path="/var/www/html"
 
 # Create the directory if it doesn't exist
 mkdir -p "$dir_path"
 
 # Write the HTML to the file
-sudo cat <<EOF > "$dir_path/$file_name"
+cat <<EOF > "$dir_path/$file_name"
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,5 +25,3 @@ sudo cat <<EOF > "$dir_path/$file_name"
 </body>
 </html>
 EOF
-
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/private/nginx-selfsigned.key -out /etc/nginx/ssl/certs/nginx-selfsigned.crt
