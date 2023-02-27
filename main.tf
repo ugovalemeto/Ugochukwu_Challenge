eee5225d260b63b48152ffc1738a88385e565b26
ugovalemeto <126273449+ugovalemeto@users.noreply.github.com>
2023-02-27 02:22:40 +0100

    Creating module

--- main.tf
+++ main.tf
@@ -5,10 +5,14 @@ resource "random_id" "random_id_prefix" {
 module "ec2" {
   source      = "./modules/ec2"
   region      = var.AWS_REGION
-  environment = var.environment
-  vpc_cidr    = var.vpc_cidr
-  #public_subnets_cidr  = var.public_subnets_cidr
-  #private_subnets_cidr = var.private_subnets_cidr
-  #availability_zone   = local.production_availability_zones
+  infra_task_sg_id = module.networking.infra_task_sg_id
+  infra_task_subnet_id = [module.networking.infra_task_public_subnet_1_id, module.networking.infra_task_public_subnet_2_id]
+  infra_task_subnet_id_1 = module.networking.infra_task_public_subnet_1_id
+  # environment = var.environment
 
+}
+
+module "networking"{
+  source = "./modules/vpc"
+  infra_task_instance_id =  module.ec2.infra_task_instance_id
 }
