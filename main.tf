960ca119ac2c5c8d047d18e3d2991ecbf47928a7
ugovalemeto <potus-mac@potus-mac.local>
2023-02-27 03:01:02 +0100

    changed checkov name

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
-resource "random_id" "random_id_prefix" {
-  byte_length = 2
-}
-
 module "ec2" {
   source      = "./modules/ec2"
   region      = var.AWS_REGION
+  infra_task_vpc_id = module.networking.infra_task_vpc_id
   infra_task_sg_id = module.networking.infra_task_sg_id
   infra_task_subnet_id = [module.networking.infra_task_public_subnet_1_id, module.networking.infra_task_public_subnet_2_id]
   infra_task_subnet_id_1 = module.networking.infra_task_public_subnet_1_id
-  # environment = var.environment
 
 }
 
